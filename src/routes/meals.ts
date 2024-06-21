a3e078906617ddd6ef44db8924fe59ea8dc5b4e7
Carolina Teixeira <maria.silva@creathus.org.br>
2024-06-20 22:17:48 -0400

    feat: creates the edit meal route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -38,4 +38,42 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(201).send()
     },
   )
+
+  app.put(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const idMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const updateMealsBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.string().datetime(),
+      })
+
+      const { id } = idMealParamsSchema.parse(request.params)
+
+      const { name, description, isOnDiet, date } = updateMealsBodySchema.parse(
+        request.body,
+      )
+
+      const mealExists = await knex('meals').where({ id }).first()
+
+      if (!mealExists) {
+        return reply.status(400).send({ error: 'Meal not found' })
+      }
+
+      await knex('meals').where({ id }).update({
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date,
+      })
+
+      return reply.status(201).send()
+    },
+  )
 }
